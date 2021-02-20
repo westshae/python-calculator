2e051568b8a28e38dfb204c68def5f5064e4e991
AuraBoom <47488174+AuraBoom@users.noreply.github.com>
2021-02-21 12:56:35 +1300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,52 +1,94 @@
-from tkinter import*
+from tkinter import *
 
-window = Tk()
-window.title("Calculator")
-operation = ""
-inputField = StringVar()
+class Calculator:
+    def __init__(self):
+        #Configuration
+        self.buttonColour = "#323333"
+        self.defaultScreenSize = "400x509"
+        self.backgroundColour = "#323333"
+        self.activeBackgroundColour="#666666"
+        self.fontInfo = ["arial", 18,"bold"]
+        self.fontColour = "#c9c9c9"
 
-# Config
-fontInfo = ["arial", 18, "bold"]
-backgroundColour = "#c7c7c7"
-buttonColour = "#323333"
-defaultScreenSize = "400x600"
-buttonToCreate = ["1","2","3","4","5","6", "7", "8", "9", "0", "+", "-", "*", "/", "=", "CLR"]
+        #Creates window with basic information.
+        self.root = Tk()
+        self.root.title("L3 DIT Calculator - Shae West")
+        self.root.geometry(self.defaultScreenSize)
+        self.root.resizable(width=False, height=False)
+        self.app = Frame(self.root, bg=self.backgroundColour)
+        self.operation=""
 
+        #Creates the input field string var and sets it to the text
+        self.inputField = StringVar()
+        self.inputField.set("Please press CLS to start")
 
-screenWidth = window.winfo_screenwidth()
-screenHeight = window.winfo_screenheight()
+        #Creates display window for calculator
+        self.entry = Entry(self.app,validate="key",font=self.fontInfo,textvariable=self.inputField, bg="gray" , width=29, bd=4, justify="center")
+        self.entry.grid(row=0, column=0, columnspan=6, ipady=10, pady=5, padx=4)
 
+        def keyPress(event):
+            self.operationList = ["1","2","3","4","5","6","7","8","9","0","+","-", "/", "*"]
+            if(event.char in self.operationList):
+                self.operation = self.operation + event.char
+            elif(event.char == "="):
+                answer(self)
+            elif(event.keysym == "Return"):
+                answer(self)
 
-window.geometry(defaultScreenSize)
-displayWindow = Entry(window, font=fontInfo, textvariable=inputField, bg=backgroundColour, justify="right", width=screenWidth).grid()
+        self.entry.bind("<Key>", keyPress)
 
-def createButton(textDisplay, rowLocation, columnLocation):
-    Button(window, padx=14, fg=buttonColour, font=fontInfo, text=textDisplay).grid(row=rowLocation, column=columnLocation)
+        #Creating buttons
+        def createButton(self, textDisplay, rowLocation, columnLocation, command):
+            self.button=Button(self.app,padx=14,pady=14, bd=4, bg=self.buttonColour, command=command,text=textDisplay, font=self.fontInfo, width=4, height=2, activebackground=self.activeBackgroundColour)
+            self.button.grid(row=rowLocation, column=columnLocation)
 
-for i in range(buttonToCreate.count):
-    print(i)
-    
+        #Adds specified text to the operation variable
+        def addToOperation(self, toAdd):
+            self.operation = self.operation + toAdd
+            self.inputField.set(self.operation)
 
+        #Calculates the answer and sets the input fieldn and operation string
+        def answer(self):
+            self.answer = eval(self.operation)
+            self.inputField.set(self.answer)
+            self.operation = str(self.answer)
+        
+        #Clears the input field, operation string and answer string
+        def clear(self):
+            self.inputField.set("")
+            self.operation = ("")
+            self.answer = ("")
 
-window.mainloop()
+        #Creates number buttons
+        createButton(self, "1", 1,0, lambda: addToOperation(self, "1"))
+        createButton(self, "2", 1,1, lambda: addToOperation(self, "2"))
+        createButton(self, "3", 1,2, lambda: addToOperation(self, "3"))
+        createButton(self, "4", 2,0, lambda: addToOperation(self, "4"))
+        createButton(self, "5", 2,1, lambda: addToOperation(self, "5"))
+        createButton(self, "6", 2,2, lambda: addToOperation(self, "6"))
+        createButton(self, "7", 3,0, lambda: addToOperation(self, "7"))
+        createButton(self, "8", 3,1, lambda: addToOperation(self, "8"))
+        createButton(self, "9", 3,2, lambda: addToOperation(self, "9"))
+        createButton(self, "0", 4,0, lambda: addToOperation(self, "0"))
 
-# cal = Tk()
-# cal.title("Calculator")
-# operation=""
-# inputField = StringVar()
+        #Creates operation buttons
+        createButton(self, "+", 1,3, lambda: addToOperation(self, "+"))
+        createButton(self, "-", 2,3, lambda: addToOperation(self, "-"))
+        createButton(self, "*", 3,3, lambda: addToOperation(self, "*"))
+        createButton(self, "/", 4,3, lambda: addToOperation(self, "/"))
+        createButton(self, "=", 4, 2, lambda: answer(self))
+        createButton(self, "CLR", 4,1, lambda: clear(self))
+        
 
-# textDisplay = Entry(cal, font=("arial", 18, "bold"), textvariable=inputField, insertwidth=5, bg="#c7c7c7", justify="right").grid(columnspan=5)
+        #Packs the app
+        self.app.pack()
 
-# button1 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="1").grid(row=1,column=1)
-# button2 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="2").grid(row=1, column=2)
-# button3 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="3").grid(row=1, column=3)
-# button4 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="4").grid(row=1, column=4)
-# button5 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="5").grid(row=2, column=1)
-# button6 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="6").grid(row=2, column=2)
-# button7 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="7").grid(row=2, column=3)
-# button5 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="8").grid(row=2, column=4)
-# button5 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="9").grid(row=3, column=1)
-# button5 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="0").grid(row=3, column=2)
-# button5 = Button(cal, padx=14, fg="black", font=("arial", 18, "bold"), text="+").grid(row=3, column=3)
+        #Runs the mainloop
+        self.root.mainloop()
 
-# cal.mainloop()
+        
+
+#Ensures that the file run is the main file.
+if __name__ == "__main__":
+    #Runs the calculator class
+    Calculator()
